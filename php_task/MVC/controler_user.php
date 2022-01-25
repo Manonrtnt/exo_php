2f7fc3e8c06f533cdf1874eff6f90378143a8a14
Manonrtnt <70939460+Manonrtnt@users.noreply.github.com>
2022-01-25 10:25:54 +0100

    add mvc task_user

---
+++ php_task/MVC/controler_user.php
@@ -0,0 +1,16 @@
+<?php
+    include('vue_user.php');
+    include('connect.php');
+
+    if (isset($_POST['name_user'])&& 
+    isset($_POST['first_name_user'])&&
+    isset($_POST['login_user'])&&
+    isset($_POST['mdp_user'])) {
+        $name_user = $_POST['name_user'];
+        $first_name_user = $_POST['first_name_user'];
+        $login_user = $_POST['login_user'];
+        $mdp_user = $_POST['mdp_user'];
+
+        include('model_user.php');
+    }
+?>

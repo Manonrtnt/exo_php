53ff8a26e81664f7ddaddd2b19610edb2fff8c43
Manonrtnt <70939460+Manonrtnt@users.noreply.github.com>
2022-02-01 09:05:57 +0100

    maj 31/01/22

--- php_guilde/controllers/registerUser.php
+++ php_guilde/controllers/registerUser.php
@@ -1,9 +1,19 @@
 <?php
+    // ajout des headers dont celui qui autorise les méthodes en POST et celui qui autorise les connexion de toute origine (différente adresse ip).
+    header("Access-Control-Allow-Origin:*");
+    header("Access-Control-Allow-Methods: POST");
+
     // appel des fichiers dont j'aurais besoin. 
     // include_once pour ne l'appeler d'une fois. 
     include_once ("../models/userbean.php");
     include ("../utils/db_config.php");
     include ("../views/register.php");
+
+    // Création de variables qui me serviront à retourner du JSON -> Traiter dans JS
+    $success = 0;
+    $msg = "";
+    $data = [];
+
 
     // Control de l'existence des champs fournis
     if(isset($_POST['pseudo'])
@@ -21,14 +31,14 @@
 
         // ensuite je vérifie que password et confPassword sont identique
         if ($password !== $confPassword){
-            echo "<p>Vos mot de passe ne sont pas identiques</p>";
+            $msg = "Vos mot de passe ne sont pas identiques";
         } else {
              // je vérifie si la valeur de $mail est bien un mail grâce à la fonction filter_var
             // qui prend la donnée en paramètre ainsi que FILTER_VALIDATE_EMAIL pour demander la vérification
             if(!filter_var($mail, FILTER_VALIDATE_EMAIL)){
-                echo "<p>Cette adresse email n'est pas valide</p>";
+                $msg = "Cette adresse email n'est pas valide";
             } else if ($mail !== $confMail){
-                echo "<p>Vos adresse mail ne sont pas identiques</p>";
+                $msg = "Vos adresse mail ne sont pas identiques";
             } else {
                 // check commentaire 
                 // supprimer les balies html strip_tags et htmlspecialchars
@@ -46,11 +56,31 @@
                 $newUser->setMail($verifMail);
 
                 // Il ne reste plus qu'à appeler la fonction createUser incluse dans la classe UserBean
-                var_dump($newUser);
-                $newUser->createUser();
+                //var_dump($newUser);
+                if ($newUser->createUser()){
+                    // Ma variale $success passe à 1    
+                    $success = 1;
+                    // Je passe un message de réussite
+                    $msg = "Utilisateur créé avec succès";
+                    // retourne (pour le moment) seulement son pseudo en donnée
+                    $data['pseudo']=$verifPseudo;
+                    //var_dump($data);            
+                } else {
+                    $msg = "Erreur lors de l'enregistrement";
+                }
             }
         }
     } else {
-        echo '<p>Veuillez renseigner tous les champs</p>';
+        $msg = 'Veuillez renseigner tous les champs';
+    }
+
+    if ($success == 1){
+        // Je créée un tableau qui contiendra success(clé) + sa valeur $success, msg et data
+        $result = ["success" => $success, "msg" => $msg, "data"=>$data];
+        echo json_encode($result);
+    } else  {
+        // retourne tableau avec success et msg
+        $result = ["success" => $success, "msg" => $msg];
+        echo json_encode($result);
     }
 ?>
